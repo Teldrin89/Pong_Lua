dae5d316f4749cac9d4681987e0090b23ef9f8af
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-03-07 00:12:52 +0100

    updated the paddles rendering given the new positions from update(dt) function

--- pong-3d/main.lua
+++ pong-3d/main.lua
@@ -74,19 +74,19 @@ function love.update(dt)
     -- player 1 movement
     if love.keyboard.isDown('w') then
         -- add negative paddle speed to current Y (scaled by dt)
-        player1Y = player1Y + -PADDLE_SPEED*dt
+        player1Y = player1Y + -PADDLE_SPEED * dt
     elseif love.keyboard.isDown('s') then
         -- add positive paddle speed to current Y (scaled by dt)
-        player1Y = player1Y + PADDLE_SPEED*dt
+        player1Y = player1Y + PADDLE_SPEED * dt
     end
 
     -- the same movement setup for player 2 (up and down keys)
     if love.keyboard.isDown('up') then
         -- add negative paddle speed to current Y (scaled by dt)
-        player2Y = player2Y + -PADDLE_SPEED*dt
+        player2Y = player2Y + -PADDLE_SPEED * dt
     elseif love.keyboard.isDown('down') then
         -- add positive paddle speed to current Y (scaled by dt)
-        player2Y = player2Y + PADDLE_SPEED*dt
+        player2Y = player2Y + PADDLE_SPEED * dt
     end
 end
 
@@ -134,10 +134,10 @@ function love.draw()
         VIRTUAL_HEIGHT/3
                     )
 
-    -- render 1st paddle - left side
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
-    -- render 2nd paddle - right side
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH-10, VIRTUAL_HEIGHT-50, 5, 20)
+    -- render 1st paddle - left side - now with player "Y" variable
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+    -- render 2nd paddle - right side - now with player "Y" variable
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH-10, player2Y, 5, 20)
     -- render ball (at center) - there is "-2" as the ball is 4x4 size
     love.graphics.rectangle('fill', VIRTUAL_WIDTH/2 -2, VIRTUAL_HEIGHT/2 -2, 
     4, 4)

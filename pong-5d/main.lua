0e0316abd5e0c09ca237932e6c0cdcaff527602f
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-03-12 23:47:26 +0100

    initial commit for update day 5 - class update

---
+++ pong-5d/main.lua
@@ -0,0 +1,32 @@
+--[[ 
+    first game devloped in lua with GD50 course using love framework
+    before running this simple example in case of 1st run with LOVE framework
+    remember to add love dictionary to PATH
+    lua is a lightweight scripting language focused around "tables" (sort of 
+    like dictionaries in Python)
+    LOVE2D is a game development framework written in c++; it uses lua as its
+    scripting language
+]]
+-- PONG game - replica of 1970s game
+
+
+--                  Game 5 update - class update
+
+
+-- setup the window width and height for the pong game
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+--[[
+    push - it is a library that will allow to draw the game at a virtual
+    resolution but a the window size that was already defined above
+    https://github.com/Ulydev/push
+]]
+push = require 'push'
+
+-- set up virtual resolution width and height
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+-- setup the pad speed variable - 200 (arbitrary value)
+PADDLE_SPEED = 200

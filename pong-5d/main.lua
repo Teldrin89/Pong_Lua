6bd008cdb1132ac24fd472d1e0779034d74e0e53
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-03-13 00:08:27 +0100

    initial commit for main.lua day 5 update - basic setup of new classes

--- pong-5d/main.lua
+++ pong-5d/main.lua
@@ -23,7 +23,33 @@ WINDOW_HEIGHT = 720
     https://github.com/Ulydev/push
 ]]
 push = require 'push'
+--[[
+    class is a sort of blueprint for object (for example a car) with 2 
+    distuguished properties: methods (e.g. refuel) and attributes (e.g. color);
+    defined class owns it's methods and properties so there is no need to define
+    seprate functions; the perfect simple cases for classes in pong will be a
+    paddle and ball as a class - put in seprate files (usually starting with
+    capital letters)
+]]
+--[[
+    the "class" library allows to represent a class in a similar way as in for
+    example java or python (classes are a lua native feature but this
+    additional library makes using an object oriented programming approach
+    easier in lua)
+    https://github.com/vrld/hump/blob/master/class.lua
+]]
+Class = require 'class'
 
+--[[
+    added the Paddle class with position, dimensions and the logic for rendering
+    of each paddle
+]]
+require 'Paddle'
+--[[
+    added the Ball class with similar atributes and functions as paddle but for 
+    the ball movement
+]]
+require 'Ball'
 -- set up virtual resolution width and height
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243

56fee78e2c551255a6ea377c04453c6538c0bf26
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-03-13 23:48:41 +0100

    ball class 1st update

--- pong-5d/Ball.lua
+++ pong-5d/Ball.lua
@@ -1,1 +1,15 @@
--- inital commment for Ball class
+--[[
+    -- Ball Class --
+    Represents a ball which will bounce back and forth between paddles and 
+    walls until it passes a left or right boundary of the screen, scoring
+    a point for the opponent
+]]
+
+-- define the name of the class object using the class library
+Ball = Class{}
+-- start defining functions for the ball class - first one is the init
+function Ball:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height

73c1e66d2ca417fb1688b5861c0caf7eea90ffc0
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-04-06 00:03:43 +0200

    added push library and functions to the main.lua

--- pong-12d/main.lua
+++ pong-12d/main.lua
@@ -1,6 +1,8 @@
 
 -- Game 12 update - prepare for release
 
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -14,11 +16,20 @@ PADDLE_SPEED = 200
 
 function love.load()
 
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, 
+    WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+
     gameState = 'start'
 
 end
 
 function love.draw()
+
+    push:apply('start')
 
     if gameState == 'start' then
         love.graphics.printf("Hello, Welcome to start!", 0, 10, VIRTUAL_WIDTH, 'center')
@@ -30,5 +41,7 @@ function love.draw()
         love.graphics.printf("Hello, Welcome to done!", 0, 10, VIRTUAL_WIDTH, 'center')
     end
 
+    push:apply('end')
+    
     love.graphics.setBackgroundColor(.157,.176,.204)
 end

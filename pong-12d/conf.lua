6d49e4c59448ffbbdda6f79b018b70834a9e7286
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-04-05 23:50:53 +0200

    changes to default window size

--- pong-12d/conf.lua
+++ pong-12d/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.identity = nil                    -- The name of the save directory (string)
-    t.version = "0.11.1"                -- The LÖVE version this game was made for (string) - changed from 0.10.2
+    t.version = "0.11.0"                -- The LÖVE version this game was made for (string) - changed from 0.10.2
     t.console = false                   -- Attach a console (boolean, Windows only)
     t.accelerometerjoystick = true      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
     t.externalstorage = false           -- True to save files (and read from the save directory) in external storage on Android (boolean) 
@@ -8,8 +8,8 @@ function love.conf(t)
  
     t.window.title = "Untitled"         -- The window title (string)
     t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
-    t.window.width = 800                -- The window width (number)
-    t.window.height = 600               -- The window height (number)
+    t.window.width = 432                -- The window width (number)
+    t.window.height = 243               -- The window height (number)
     t.window.borderless = false         -- Remove all border visuals from the window (boolean)
     t.window.resizable = false          -- Let the window be user-resizable (boolean)
     t.window.minwidth = 1               -- Minimum window width if the window is resizable (number)

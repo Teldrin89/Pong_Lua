3cd75234744841fbae4e7fed02ca6495ac1b5583
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-04-06 12:41:12 +0200

    added update function

--- pong-12d/main.lua
+++ pong-12d/main.lua
@@ -45,27 +45,104 @@ function love.load()
 
 end
 
+function love.update(dt)
+    if gameState == 'serve' then
+        ball.dy = math.random(-50, 50)
+        if servingPlayer == 1 then
+            ball.dx = math.random(140, 200)
+        else
+            ball.dx = -math.random(140, 200)
+        end
+    elseif gameState == 'play' then
+        if ball:collides(player1) then
+            ball.dx = -ball.dx * 1.03
+            ball.x = player1.x + 5
+            if ball.dy < 0 then
+                ball.dy = -math.random(30,140)
+            else
+                ball.dy = math.random(30, 140)
+            end
+        end
+
+        if ball:collides(player2) then
+            ball.dx = -ball.dx * 1.03
+            ball.x = player2.x - 4
+            if ball.dy < 0 then
+                ball.dy = -math.random(30,140)
+            else
+                ball.dy = math.random(30, 140)
+            end
+        end
+
+        if ball.y <= 0 then
+            ball.y = 0
+            ball.dy = -ball.dy
+        end
+
+        if ball.y > VIRTUAL_HEIGHT - 4 then
+            ball.y = VIRTUAL_HEIGHT - 4
+            ball.dy = -ball.dy
+        end
+
+        if ball.x < 0 then
+            servingPlayer = 1
+            player2Score = player2Score + 1
+            if player2Score == 10 then
+                winningPlayer = 2
+                gameState = 'done'
+            else
+                gameState = 'serve'
+                ball:reset()
+            end
+        end
+
+        if ball.x > VIRTUAL_WIDTH then
+            servingPlayer = 2
+            player1Score = player1Score + 1
+            if player1Score == 10 then
+                winningPlayer = 1
+                gameState = 'done'
+            else
+                gameState = 'serve'
+                ball:reset()
+            end
+        end
+    end
+
+    if love.keyboard.isDown('w') then
+        player1.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('s') then
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
+    end
+
+    if love.keyboard.isDown('up') then
+        player2.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('down') then
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
+    end
+    
+    if gameState == 'play' then
+        ball:update(dt)
+    end
+
+    player1:update(dt)
+    player2:update(dt)
+end
+
 function love.keypressed(key)
-    -- defining quit game key - escape
     if key == 'escape' then
-        -- event.quit - a simple function that terminates application
         love.event.quit()
-    -- defining start game key - enter/return - changes state to "play"
     elseif key == 'enter' or key == 'return' then
-        -- if the game state is 'start' then set it to 'play'
         if gameState == 'start' then
             gameState = 'play'
-        -- otherwise set game state to start and set initial values
         else
             gameState = 'start'
-            -- set ball's starting position - middle of the screen
             ballX = VIRTUAL_WIDTH/2 - 2
             ballY = VIRTUAL_HEIGHT/2 - 2
-            --[[
-                set ball's initial velocity in X and Y direction using the
-                math.random and using the same ternary operation as in load 
-                function
-            ]]
             ballDX = math.random(2) == 1 and 100 or -100
             ballDY = math.random(-50, 50)
         end
@@ -88,11 +165,11 @@ function love.draw()
         love.graphics.printf("Good luck!", 0, 10, VIRTUAL_WIDTH, 'center')
     elseif gameState == 'done' then
         love.graphics.setFont(mediumFont)
-        love.graphics.printf("Player " .. tostring(servingPlayer) .. "wins!", 0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf("Player " .. tostring(winningPlayer) .. "wins!", 0, 10, VIRTUAL_WIDTH, 'center')
         love.graphics.setFont(smallFont)
         love.graphics.printf("Press Enter to start over", 0, 30, VIRTUAL_WIDTH, 'center')
     end
-    
+
     player1:render()
     player2:render()
     ball:render()
@@ -103,8 +180,6 @@ function love.draw()
 end
 
 function displayScore()
-    -- draw score on the left and right center of the screen
-    --change font to score font
     love.graphics.setFont(scoreFont)
     love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH/2 - 50,
         VIRTUAL_HEIGHT/3)
